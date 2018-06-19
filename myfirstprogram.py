82fe9bb7c196c917ccfd2bce550807cc21fe19de
Kimberly Horan <codinglikeagirl42@gmail.com>
2018-06-19 14:10:06 +0000

    First commit

---
+++ myfirstprogram.py
@@ -0,0 +1,18 @@
+#comment goes here
+#my first program
+
+
+name = "Vahdeta Suljic"
+print name[1:5]
+
+request = "abcbd"
+print request.split('b')
+
+res = "respect"
+print "-".join("respect")
+print res.upper()
+print name[-13]
+print name[::-1]
+
+fullname = name.split()
+print fullname
